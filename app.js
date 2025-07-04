7f542512214968633f0fc111f41f83ec77bb280e
goby35 <hongngoc30524@gmail.com>
2025-07-04 10:12:58 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+// Theem o buoc 6
+const ApiError = require('./app/api-error');
 
 // Theem o buoc 5
 const contactRouter = require('./app/routes/contact.route');
@@ -11,6 +13,20 @@ app.use(express.json());
 
 // Theem o buoc 5
 app.use('/api/contacts', contactRouter);
+
+// Theem o buoc 6
+app.use((req, res, next) => {
+    // Code ở đây sẽ chay khi không có route nào khớp
+    // với yêu cầu của người dùng
+    return next(new ApiError(404, 'Resource not found'));
+});
+// Theem o buoc 6
+//Define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || 'Internal Server Error',
+    });
+});
 
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to contact book application' });
