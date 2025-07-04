8db80844b6e3ecd44f177b7763041642597f28fd
goby35 <hongngoc30524@gmail.com>
2025-07-04 09:47:27 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 
+// Theem o buoc 5
+const contactRouter = require('./app/routes/contact.route');
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Theem o buoc 5
+app.use('/api/contacts', contactRouter);
 
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to contact book application' });
